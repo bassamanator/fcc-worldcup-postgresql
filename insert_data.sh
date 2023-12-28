dbf26dff161af50dc2e56a94925b471cd663f328
Bassam Husain <bassam.husain@gmail.com>
2023-12-28 05:58:38 -0500

    Games adding.

--- insert_data.sh
+++ insert_data.sh
@@ -13,6 +13,8 @@ fi
 
 echo $($PSQL "truncate teams, games;")
 
+echo -e "\n~~ Populating teams table ~~"
+
 cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS; do
   if [[ $YEAR != year ]]; then
     # echo $YEAR $ROUND $WINNER $OPPONENT $WINNER_GOALS $OPPONENT_GOALS
@@ -33,6 +35,22 @@ cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPON
       INSERT_RESULT=$($PSQL "insert into teams(name) values('$OPPONENT')")
       if [[ $INSERT_RESULT == "INSERT 0 1" ]]; then
         echo "Inserted into teams, $OPPONENT"
+      fi
+    fi
+  fi
+done
+
+echo -e "\n~~ Populating games table ~~"
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS; do
+  if [[ $YEAR != year ]]; then
+    WINNER_ID=$($PSQL "select team_id from teams where name='$WINNER'")
+    OPPONENT_ID=$($PSQL "select team_id from teams where name='$OPPONENT'")
+    FOUND=$($PSQL "select count(*) from games where year=$YEAR and round='$ROUND' and winner_id=$WINNER_ID and opponent_id=$OPPONENT_ID and winner_goals=$WINNER_GOALS and opponent_goals=$OPPONENT_GOALS")
+    if [[ "$FOUND" == 0 ]]; then
+      INSERT_RESULT=$($PSQL "insert into games(year,round,winner_goals,opponent_goals,winner_id,opponent_id) values($YEAR,'$ROUND',$WINNER_GOALS,$OPPONENT_GOALS,$WINNER_ID,$OPPONENT_ID)")
+      if [[ $INSERT_RESULT == "INSERT 0 1" ]]; then
+        echo "Inserting into games, $YEAR,$ROUND,$WINNER_GOALS,$OPPONENT_GOALS,$WINNER_ID,$OPPONENT_ID"
       fi
     fi
   fi
